b8cd5188e2a7cc4d69423a967e8ca171908d09c0
cameronbracken <cameron.bracken@gmail.com>
2009-09-20 14:50:42 -0600

    Beginning writeup of problem 1, blocfit matches locfit!

---
+++ data/1-localRegression/RSession1.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+system("R CMD INSTALL --no-docs blocfit")
+require(blocfit)
+#require(locfit)
+#data(ethanol)
+data(oneD)
+
+x <- oneD$x; y <- oneD$y
+#x <- ethanol$E; y <- ethanol$NOx
+
+blfit <- blocfit(x, y, a=.7, p=2, kern='bisq')
