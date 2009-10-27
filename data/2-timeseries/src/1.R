75f96ae15eb633b503b7ba6f1709102bb1d3687f
cameronbracken <cameron.bracken@gmail.com>
2009-10-27 14:13:11 -0600

    Cool spagatti plots

--- data/2-timeseries/src/1.R
+++ data/2-timeseries/src/1.R
@@ -2,16 +2,17 @@
 source('lib.R')
 
 	#read data (ac-ft)
-x <- read.table('data/Leesferry-mon-data.txt')
+x <- as.matrix(read.table('data/Leesferry-mon-data.txt')[,-1])
 
 	# creat timeseries and convert to cms 
-x <- matrix(as.vector(t(x[,-1])),ncol=12)*0.000469050
+x <- x*0.000469050
 
 	#remove mean
 x.mean <- apply(x,2,mean)
 x.sd <- apply(x,2,sd)
 
 x <- t((t(x) - x.mean) / x.sd)
-x <- ts(array(t(x)),start=c(1906,1),frequency=12)
+x.may <- x[,5]
+x.ts <- ts(array(t(x)),start=c(1906,1),frequency=12)
 
-save(x,file='output/1.Rdata')
+save(x,x.ts,x.may,file='output/1.Rdata')
